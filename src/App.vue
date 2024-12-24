3a23d34b878b696381d824a4f2bfe6ac9e95a210
lis531 <82408934+lis531@users.noreply.github.com>
2024-12-24 22:00:21 +0100

    Update
    Unchecking opacity checkbox will revert to last value, improved how images are displayed (now the image on the left dictates size), added icon to the slider, improved appearance

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@ const image2 = ref('');
 
 const handleImage1 = (e) => {
   image1.value = URL.createObjectURL(e.target.files[0]);
+  const img = new Image();
+  img.src = image1.value;
+  img.onload = () => {
+    const comparator = document.querySelector('.comparator');
+    comparator.style.aspectRatio = `${img.width / img.height}`;
+    comparator.style.width = 'auto';
+  };
 };
 
 const handleImage2 = (e) => {
@@ -16,8 +23,10 @@ const handleRange = (e) => {
   const value = e.target.value;
   const img1 = document.querySelector('.left img');
   const img2 = document.querySelector('.right img');
+  const borderImg = document.querySelector('.borderImg');
   img1.style.clipPath = `inset(0 ${100 - value}% 0 0)`;
   img2.style.clipPath = `inset(0 0 0 ${value - 100}%)`;
+  borderImg.style.width = `calc(${value}% + 2px)`;
 };
 
 let changeFade = false;
@@ -29,6 +38,12 @@ const handleSwitch = (e) => {
     const img2 = document.querySelector('.right img');
     img1.style.opacity = 1;
     img2.style.opacity = 1;
+  } else {
+    const img1 = document.querySelector('.left img');
+    const img2 = document.querySelector('.right img');
+    const value = document.querySelector('#opacity').value;
+    img1.style.opacity = value / 100;
+    img2.style.opacity = 1 - value / 100;
   }
 };
 
@@ -56,6 +71,13 @@ onMounted(() => {
       const file = e.dataTransfer.files[0];
       if (div.classList.contains('left')) {
         image1.value = URL.createObjectURL(file);
+        const img = new Image();
+        img.src = image1.value;
+        img.onload = () => {
+          const comparator = document.querySelector('.comparator');
+          comparator.style.aspectRatio = `${img.width / img.height}`;
+          comparator.style.width = 'auto';
+        };
       } else {
         image2.value = URL.createObjectURL(file);
       }
@@ -71,19 +93,21 @@ onMounted(() => {
   </header>
   <main>
     <div class="desc">
-      <h2>Upload images</h2>
       <p>Upload two images to compare them.</p>
     </div>
-    <label>Opacity?</label>
-    <input type="checkbox" @change="handleSwitch" />
-    <input type="range" min="0" max="100" @input="handleFade" />
+    <div class="opacityControls">
+      <label v-if="image1 && image2">Opacity?</label>
+      <input v-if="image1 && image2" type="checkbox" @change="handleSwitch" />
+      <input id="opacity" v-if="image1 && image2" type="range" min="0" max="100" @input="handleFade" />
+    </div>
     <div class="comparator">
-      <input type="range" min="0" max="100" @input="handleRange" />
+      <input v-if="image1 && image2" type="range" min="0" max="100" @input="handleRange" />
       <div class="comparatorMain">
-          <div class="left">
-            <img v-if="image1" :src="image1" alt="Image 1" />
-            <input v-if="!image1" type="file" accept="image/*" @change="handleImage1" />
-          </div>
+        <div class="left">
+          <img v-if="image1" :src="image1" alt="Image 1" />
+          <div v-if="image1" class="borderImg"></div>
+          <input v-if="!image1" type="file" accept="image/*" @change="handleImage1" />
+        </div>
         <div class="right">
           <img v-if="image2" :src="image2" alt="Image 2" />
           <input v-if="!image2" type="file" accept="image/*" @change="handleImage2" />
@@ -102,6 +126,10 @@ header {
 
 main {
   width: 90vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
 .desc {
@@ -109,25 +137,44 @@ main {
   text-align: center;
 }
 
+.opacityControls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 2rem;
+}
+
 .comparator {
   display: flex;
+  align-items: center;
   position: relative;
-  overflow: hidden;
-  width: 100%;
   height: 75vh;
+  width: 100%;
+  max-width: 100%;
+  border-radius: 0.3rem;
+  overflow: visible;
 }
+
 .comparator > input {
   position: absolute;
   z-index: 100;
-  width: 100%;
+  width: calc(100% + 35px);
+  -webkit-appearance: none;
+  background: transparent;
+  left: -17.5px;
 }
 
 .comparator > input::-webkit-slider-thumb {
-  height: 100%;
-}
-
-.comparator > input:focus {
   cursor: ew-resize;
+  -webkit-appearance: none;
+  appearance: none;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  background-image: url("public/arrows.svg");
+  background-color: rgba(28, 28, 28, 0.5);
+  background-size: 100%;
+  border: 2px solid rgba(20, 20, 20, 0.5);
 }
 
 .comparatorMain {
@@ -164,6 +211,14 @@ main {
   clip-path: inset(0 0 0 50%);
 }
 
+.borderImg {
+  position: absolute;
+  width: 50%;
+  height: 100%;
+  border-right: 2px solid rgba(28, 28, 28, 0.5);
+  left: 0;
+  z-index: 15;
+}
 
 .left > input, .right > input {
   z-index: 1000;
